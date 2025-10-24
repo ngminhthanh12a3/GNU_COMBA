642c469dfc03dd80e26f76f8483347c3797f3cff
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2025-10-25 04:42:07 +0700

    temp backup for training

--- src/main.py
+++ src/main.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python
+import os, glob
+SRCDIR = os.path.realpath(os.path.dirname(__file__))
+prompt = ""
 
-print("Hello World")
+with open(SRCDIR + '/prompt.txt', 'r') as file:
+	prompt = file.read()
+
+
+VerilogEvalDatasetDir = os.path.realpath(SRCDIR + '/../ext/verilog-eval/dataset_code-complete-iccad2023')
+
+VerilogEvalDatasetPromts = glob.glob(VerilogEvalDatasetDir + '/*prompt.txt')
+VerilogEvalDatasetPromts.sort()
+
+for datasetpromptdir in VerilogEvalDatasetPromts:
+
+	datasetprompt = ""
+	with open(datasetpromptdir, 'r') as file:
+		datasetprompt = file.read()
+
+	datasetPromtFilename = os.path.basename(datasetpromptdir)
+	# datasetPromtDirName = os.path.dirname(datasetpromptdir)
+	datasetRefFileName = datasetPromtFilename.replace('prompt.txt', 'ref.sv')
+	datasetRefDir = VerilogEvalDatasetDir + '/' + datasetRefFileName
+
+	datasetRefPrompt = ""
+	with open(datasetRefDir, 'r') as file:
+		datasetRefPrompt = file.read()
+
+	print(f"""{prompt}
+
+{datasetprompt}
+
+The reference code for the module is:
+
+{datasetRefPrompt}""")
+	
+	break
+	
